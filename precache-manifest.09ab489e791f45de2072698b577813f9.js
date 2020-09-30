a498cacc6db1ef08cf30242621bcdb0f240c9235
coddielam <edlam711@gmail.com>
2020-09-30 13:18:20 +0800

    Updates

---
+++ precache-manifest.09ab489e791f45de2072698b577813f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bbc670d3924f29e1f4760d75c9e85c",
+    "url": "/react_hackernews/index.html"
+  },
+  {
+    "revision": "04aac019ccd4848fe855",
+    "url": "/react_hackernews/static/css/main.b98a0041.chunk.css"
+  },
+  {
+    "revision": "aa1b03b0a41ab6d5623d",
+    "url": "/react_hackernews/static/js/2.36b4c9c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hackernews/static/js/2.36b4c9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04aac019ccd4848fe855",
+    "url": "/react_hackernews/static/js/main.13bc9c06.chunk.js"
+  },
+  {
+    "revision": "4150249526ee4d591c4b",
+    "url": "/react_hackernews/static/js/runtime-main.32121093.js"
+  }
+]);
